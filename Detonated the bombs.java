cf68aba129ea9d582a0f6252dbb40739e83dfcb5
Sowndar K <84360457+KSOWNDAR@users.noreply.github.com>
2022-03-01 05:20:51 +0530

    Create Detonated the bombs.java

---
+++ Detonated the bombs.java
@@ -0,0 +1,79 @@
+You are given a list of bombs. The range of a bomb is defined as the area where its effect can be felt. This area is in the shape of a circle with the center as the location of the bomb.
+
+The bombs are represented by a 0-indexed 2D integer array bombs where bombs[i] = [xi, yi, ri]. xi and yi denote the X-coordinate and Y-coordinate of the location of the ith bomb, whereas ri denotes the radius of its range.
+
+You may choose to detonate a single bomb. When a bomb is detonated, it will detonate all bombs that lie in its range. These bombs will further detonate the bombs that lie in their ranges.
+
+Given the list of bombs, return the maximum number of bombs that can be detonated if you are allowed to detonate only one bomb.
+
+ 
+
+Example 1:
+
+
+Input: bombs = [[2,1,3],[6,1,4]]
+Output: 2
+Explanation:
+The above figure shows the positions and ranges of the 2 bombs.
+If we detonate the left bomb, the right bomb will not be affected.
+But if we detonate the right bomb, both bombs will be detonated.
+So the maximum bombs that can be detonated is max(1, 2) = 2.
+Example 2:
+
+
+Input: bombs = [[1,1,5],[10,10,5]]
+Output: 1
+Explanation:
+Detonating either bomb will not detonate the other bomb, so the maximum number of bombs that can be detonated is 1.
+Example 3:
+
+
+Input: bombs = [[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]]
+Output: 5
+Explanation:
+The best bomb to detonate is bomb 0 because:
+- Bomb 0 detonates bombs 1 and 2. The red circle denotes the range of bomb 0.
+- Bomb 2 detonates bomb 3. The blue circle denotes the range of bomb 2.
+- Bomb 3 detonates bomb 4. The green circle denotes the range of bomb 3.
+Thus all 5 bombs are detonated.
+  
+  
+class Solution {
+    public int maximumDetonation(int[][] bombs) {
+        int maxbombs=0;
+        for(int[] startbomb:bombs)
+        {
+            HashSet<String> detonated=new HashSet<String>();
+            detonated.add(Stringify(startbomb));
+            maxbombs=Math.max(maxbombs,detonatedBombs(startbomb,detonated,bombs));
+            if(maxbombs==bombs.length) return maxbombs;
+        }
+        return maxbombs;
+    }
+    private int detonatedBombs(int[] startbomb,HashSet<String> detonated,int[][] bombs)
+    {
+        for(int[] testBombs:bombs)
+        {
+            String BombRef=Stringify(testBombs);
+            if(!detonated.contains(BombRef))
+            {
+                if(findDistance(startbomb,testBombs)<=startbomb[2])
+                {
+                    detonated.add(BombRef);
+                    detonatedBombs(testBombs,detonated,bombs);
+                }
+            }
+        }
+        return detonated.size();
+    }
+    private double findDistance(int[]  point1,int[] point2)
+    {
+        double Xdist=Math.abs(point1[0]-point2[0]);
+        double Ydist=Math.abs(point1[1]-point2[1]);
+        return Math.hypot(Xdist,Ydist);
+    }
+    private String Stringify(int[] bomb)
+    {
+        return bomb.toString();
+    }
+}
