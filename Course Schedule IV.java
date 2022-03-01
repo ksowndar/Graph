5187447764002c28f6bc36bd6cb600e9c812866c
Sowndar K <84360457+KSOWNDAR@users.noreply.github.com>
2022-03-01 10:23:46 +0530

    Create Course Schedule IV.java

---
+++ Course Schedule IV.java
@@ -0,0 +1,63 @@
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course ai first if you want to take course bi.
+
+For example, the pair [0, 1] indicates that you have to take course 0 before you can take course 1.
+Prerequisites can also be indirect. If course a is a prerequisite of course b, and course b is a prerequisite of course c, then course a is a prerequisite of course c.
+
+You are also given an array queries where queries[j] = [uj, vj]. For the jth query, you should answer whether course uj is a prerequisite of course vj or not.
+
+Return a boolean array answer, where answer[j] is the answer to the jth query.
+
+ 
+
+Example 1:
+
+
+Input: numCourses = 2, prerequisites = [[1,0]], queries = [[0,1],[1,0]]
+Output: [false,true]
+Explanation: The pair [1, 0] indicates that you have to take course 1 before you can take course 0.
+Course 0 is not a prerequisite of course 1, but the opposite is true.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [], queries = [[1,0],[0,1]]
+Output: [false,false]
+Explanation: There are no prerequisites, and each course is independent.
+Example 3:
+
+
+Input: numCourses = 3, prerequisites = [[1,2],[1,0],[2,0]], queries = [[1,0],[1,2]]
+Output: [true,true]
+ 
+
+class Solution {
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        boolean[][] graph=new boolean[numCourses][numCourses];
+        
+        for(int[] p:prerequisites)
+        {
+            graph[p[0]][p[1]]=true;
+        }
+        int i,j,k;
+        for(k=0;k<numCourses;k++)
+        {
+            for(i=0;i<numCourses;i++)
+            {
+                if(k==i) continue;
+            for(j=0;j<numCourses;j++)
+            {
+                if(i==j || j==k) continue;
+                
+                graph[i][j]=graph[i][j] || graph[i][k] && graph[k][j];
+            }
+            }
+        }
+        
+        List<Boolean>list=new ArrayList<>();
+        
+        for(int[] q:queries)
+        {
+            list.add(graph[q[0]][q[1]]);
+        }
+        
+        return list;
+    }
+}
